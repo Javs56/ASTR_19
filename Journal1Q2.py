b5236e73c7e02c7cdde2a79e1dd3a9fdce3f72f6
Javs56 <146606196+Javs56@users.noreply.github.com>
2024-10-20 12:05:52 -0700

    Create Journal1Q2.py

---
+++ Journal1Q2.py
@@ -0,0 +1,15 @@
+a = 2.3
+b = 7.6
+c = a + b
+print(a, '+', b, 'is', c, '\n and its data type is')
+print(type(c), '\n')
+
+d = 5
+e = 10
+f = d-e
+print(d, '-', e, 'is', f, '\n and its data type is')
+print(type(f), '\n')
+
+g = e*b
+print(e, 'x', b, 'is', g, '\n and its data type is')
+print(type(g), '\n')
