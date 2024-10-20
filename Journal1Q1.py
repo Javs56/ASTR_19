410691bbc6f923192790ea4610d178c1a8732be2
Javs56 <146606196+Javs56@users.noreply.github.com>
2024-10-20 12:28:12 -0700

    Add files via upload

--- Journal1Q1.py
+++ Journal1Q1.py
@@ -1,6 +1,3 @@
-#Write a Python program to print out your full name, your preferred pronouns (optional), 
-#and two sentences about your favorite movie and your favorite food.
-
 who_am_i = "Hello! My name is Javier Sanchez, and my pronouns are he/him. My favorite dish are flautas, which are fried tacos. My favorite movie is WALL-E"
 print(who_am_i)
 
