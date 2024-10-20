7ae6725e7e01cf24a4c2839e7b31d80c49517d4c
Javs56 <146606196+Javs56@users.noreply.github.com>
2024-10-20 12:17:03 -0700

    Update Journal1Q2.py

--- Journal1Q2.py
+++ Journal1Q2.py
@@ -1,3 +1,7 @@
+#Write a Python that prints the sum of two floating point numbers, the difference between two integers, 
+#and the product of a floating point number and an integer. In each case, have the program print out the 
+#data type of the resulting answer.
+
 a = 2.3
 b = 7.6
 c = a + b
